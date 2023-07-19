a03d960bb7fd74bb61df6d0ae734a4205d531aa1
Dilshodbek0720 <sayitqulovdilshodbek@gmail.com>
2023-07-19 18:22:21 +0500

    clear main

---
+++ lib/tab_box_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class TabBoxScreen extends StatefulWidget {
+  const TabBoxScreen({super.key});
+
+  @override
+  State<TabBoxScreen> createState() => _TabBoxScreenState();
+}
+
+class _TabBoxScreenState extends State<TabBoxScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TabBox"),
+      ),
+    );
+  }
+}
